75a6aeeca2cd75a349434b5b883deec9c95c1260
@filippov_r <filippov.r@vipavenue.ru>
2026-08-28 11:41:25 +0300

    ci: add GitHub Actions deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/blog-constructor/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
